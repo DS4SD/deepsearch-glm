d3d53f4867e0f96f88dee57b04825e3e9ef18b7e
Peter Staar <taa@zurich.ibm.com>
2023-10-18 07:48:49 +0200

    added NLP tests to package
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- deepsearch_glm/utils/load_pretrained_models.py
+++ deepsearch_glm/utils/load_pretrained_models.py
@@ -15,32 +15,46 @@ def load_pretrained_nlp_models(force:bool=False, verbose:bool=False):
     
     with open(f"{RESOURCES_DIR}/models.json") as fr:
         models = json.load(fr)
+
+    print(models)
         
     COS_URL = models["object-store"]
     COS_PRFX = models["nlp"]["prefix"]
     COS_PATH = os.path.join(COS_URL, COS_PRFX)
     
-    cmds=[]
+    cmds={}
     for name,files in models["nlp"]["trained-models"].items():
         source = os.path.join(COS_PATH, files[0])
         target = os.path.join(RESOURCES_DIR, files[1])
 
         cmd = ["curl", source, "-o", target, "-s"]
-        cmds.append(cmd)
+        cmds[name] = cmd
+
+    models=[]
         
-    for cmd in cmds:
+    for name,cmd in cmds.items():
 
         model_weights = cmd[3]
         
         if force or (not os.path.exists(model_weights)):
 
             if verbose:
-                print(f"downloading {os.path.basename(model_weights)} ... ", end="")            
+                #print(f"downloading {os.path.basename(model_weights)} ... ", end="")
+                print(f"downloading {name} ... ", end="")            
 
             message = subprocess.run(cmd)    
 
             if verbose:
                 print("done!")
-                
-        elif verbose:
-            print(f" -> already downloaded {os.path.basename(cmd[3])}")
+            models.append(name)
+
+        elif os.path.exists(model_weights):
+            if verbose:
+                #print(f" -> already downloaded {os.path.basename(cmd[3])}")
+                print(f" -> already downloaded {name}")
+            models.append(name)
+            
+        else:
+            print(f" -> missing {name}")
+            
+    return models
