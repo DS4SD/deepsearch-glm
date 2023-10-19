664d22589ce84e97b358fef1d4f7888003bcd82b
Peter Staar <taa@zurich.ibm.com>
2023-10-19 07:20:50 +0200

    added NLP tests on full documents
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -2,7 +2,8 @@
 import os
 import json
 
-from deepsearch_glm.nlp_utils import list_nlp_model_configs, init_nlp_model
+from deepsearch_glm.nlp_utils import list_nlp_model_configs, init_nlp_model, \
+    extract_references_from_doc
 from deepsearch_glm.utils.load_pretrained_models import load_pretrained_nlp_models
 
 def test_01_load_nlp_models():
@@ -52,7 +53,7 @@ def test_02B_run_nlp_models_on_text():
 
 def test_03A_run_nlp_models_on_document():
 
-    with open("./data/documents/articles/1806.02284.json") as fr:
+    with open("./tests/data/docs/1806.02284.json") as fr:
         doc = json.load(fr)
     
     model = init_nlp_model("sentence;language;term;reference")
@@ -70,7 +71,7 @@ def test_03A_run_nlp_models_on_document():
     
 def test_03B_run_nlp_models_on_document():
 
-    with open("./data/documents/articles/1806.02284.json") as fr:
+    with open("./tests/data/docs/1806.02284.json") as fr:
         doc = json.load(fr)
 
     filters = ["properties"]
@@ -89,3 +90,102 @@ def test_03B_run_nlp_models_on_document():
         assert label not in res
                   
     check_dimensions(res["properties"])
+
+def test_03C_run_nlp_models_on_document():
+
+    model = init_nlp_model("language;semantic;sentence;term;verb;conn;geoloc;reference")
+
+    source = "./tests/data/docs/1806.02284.json"
+    target = "./tests/data/docs/1806.02284.nlp.json"
+    
+    if False: # generate the test-data
+        with open(source) as fr:
+            doc = json.load(fr)
+
+        res = model.apply_on_doc(doc)
+        extract_references_from_doc(res)
+        
+        fw = open(target, "w")
+        fw.write(json.dumps(res)+"\n")            
+        fw.close()
+        
+        assert True
+        
+    else:
+        with open(source) as fr:
+            sdoc = json.load(fr)
+
+        res = model.apply_on_text(sdoc)        
+
+        with open(target) as fr:
+            tdoc = json.load(fr)
+        
+        assert res==tdoc
+    
+def test_04A_terms():
+
+    model = init_nlp_model("language;semantic;sentence;term;verb;conn;geoloc")
+
+    source = "./tests/data/texts/terms.jsonl"
+    target = "./tests/data/texts/terms.nlp.jsonl"
+    
+    if False: # generate the test-data
+        with open(source) as fr:
+            lines = fr.readlines()
+
+        fw = open(target, "w")
+    
+        for line in lines:
+            data = json.loads(line)
+            res = model.apply_on_text(data["text"])
+
+            fw.write(json.dumps(res)+"\n")
+            
+        fw.close()
+
+    else:
+        with open(target) as fr:
+            lines = fr.readlines()
+            
+        for line in lines:
+            data = json.loads(line)
+            res = model.apply_on_text(data["text"])
+
+            assert res==data
+       
+    assert True
+    
+def test_04B_references():
+
+    model = init_nlp_model("reference")
+
+    source = "./tests/data/texts/references.jsonl"
+    target = "./tests/data/texts/references.nlp.jsonl"
+    
+    if False: # generate the test-data
+        
+        with open(source) as fr:
+            lines = fr.readlines()
+
+        fw = open(target, "w")
+    
+        for line in lines:
+            data = json.loads(line)
+            res = model.apply_on_text(data["text"])
+            
+            fw.write(json.dumps(res)+"\n")
+                
+        fw.close()
+        assert True
+        
+    else:
+        with open(target) as fr:
+            lines = fr.readlines()
+            
+        for line in lines:
+            data = json.loads(line)
+            res = model.apply_on_text(data["text"])
+
+            assert res==data
+    
+
