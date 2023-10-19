80c2c97e9b2a9a3e3c0d6cc18ad29a4036b948a2
Peter Staar <taa@zurich.ibm.com>
2023-10-19 05:57:52 +0200

    added more comprehensive tests
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,13 +1,11 @@
 
+import os
+import json
+
 from deepsearch_glm.nlp_utils import list_nlp_model_configs, init_nlp_model
 from deepsearch_glm.utils.load_pretrained_models import load_pretrained_nlp_models
 
-def test_1():
-    x = 1
-    y = 1
-    assert x==y
-
-def test_2():
+def test_01_load_nlp_models():
     models = load_pretrained_nlp_models()
     print(f"models: {models}")
 
@@ -15,13 +13,75 @@ def test_2():
     assert "semantic" in models
     assert "name" in models
     assert "reference" in models
-    
-def test_3():
+
+def check_dimensions(item):
+
+    assert "headers" in item
+    assert "data" in item    
+
+    headers = item["headers"]
+    for row in item["data"]:
+        assert len(row)==len(headers)
+
+def test_02A_run_nlp_models_on_text():
 
     model = init_nlp_model("sentence;language;term")
     res = model.apply_on_text("FeSe is a material.")
 
-    assert "text" in res
+    for label in ["text", "properties", "instances", "relations"]:
+        assert label in res        
+        
+def test_02B_run_nlp_models_on_text():
 
-    assert "properties" in res
-    assert "instances" in res
+    filters = ["properties"]
+    
+    model = init_nlp_model("sentence;language;term")
+    res = model.apply_on_text("FeSe is a material.", filters)
+
+    for label in ["text", "properties"]:
+        assert label in res
+
+    for label in ["instances", "relations"]:
+        assert label not in res        
+
+def test_03A_run_nlp_models_on_document():
+
+    with open("./data/documents/articles/1806.02284.json") as fr:
+        doc = json.load(fr)
+    
+    model = init_nlp_model("sentence;language;term;reference")
+    res = model.apply_on_doc(doc)
+    print(res.keys())
+
+    for label in ["description", "body", "meta",
+                  "page-elements", "texts", "tables", "figures",
+                  "properties", "instances", "relations"]:
+        assert label in res
+
+    check_dimensions(res["properties"])
+    check_dimensions(res["instances"])
+    check_dimensions(res["relations"])
+    
+def test_03B_run_nlp_models_on_document():
+
+    with open("./data/documents/articles/1806.02284.json") as fr:
+        doc = json.load(fr)
+
+    filters = ["properties"]
+        
+    model = init_nlp_model("sentence;language;term;reference")
+    res = model.apply_on_doc(doc, filters)
+    print(res.keys())
+
+    for label in ["dloc", "applied-models",
+                  "description", "body", "meta",
+                  "page-elements", "texts", "tables", "figures",
+                  "properties"]: 
+        assert label in res
+
+    for label in ["instances", "relations"]:
+        assert label not in res
+                  
+    check_dimensions(res["properties"])
+    check_dimensions(res["instances"])
+    check_dimensions(res["relations"])
