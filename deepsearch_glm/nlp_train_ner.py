e7c642276d52569d1cef9ce156ec42ea7a253e32
Peter Staar <taa@zurich.ibm.com>
2023-12-03 11:46:50 +0100

    added a simple crf-trainer
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

---
+++ deepsearch_glm/nlp_train_ner.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+import time
+import json
+import glob
+import tqdm
+import argparse
+
+import random
+
+import subprocess
+
+import numpy as np
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import textColor as tc
+
+from tabulate import tabulate
+from deepsearch_glm.andromeda_nlp import nlp_model
+
+from deepsearch_glm.utils.ds_utils import convert_pdffiles
+from deepsearch_glm.nlp_utils import create_nlp_dir, init_nlp_model
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(
+        prog = "nlp_train_reference",
+        description = 'Prepare CRF data for CRF-reference parser',
+        epilog =
+"""
+examples of execution: 
+
+1. end-to-end example on pdf documents:
+
+    poetry run python ./deepsearch_glm/nlp_train_semantic.py -m all --input-dir '<root-dir-of-json-docs> --output-dir <models-directory>'
+""",
+        formatter_class=argparse.RawTextHelpFormatter)
+        
+    parser.add_argument('-m', '--mode', required=True, default="all",
+                        help="mode for training semantic model",
+                        choices=["prepare","train","all"])
+
+    parser.add_argument('--input-dir', required=False,
+                        type=str, default=None,
+                        help="input directory with documents with `train.jsonl and `test.jsonl``")
+    
+    parser.add_argument('--output-dir', required=False,
+                        type=str, default="./reference-models",
+                        help="output directory for trained models & annotated data")
+
+    parser.add_argument('--max-items', required=False,
+                        type=int, default=-1,
+                        help="number of references")
+    
+    args = parser.parse_args()
+
+    idir = args.input_dir
+    
+    if args.output_dir==None:
+        odir = create_nlp_dir()
+    
+    elif not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+        odir = args.output_dir
+
+    else:
+        odir = args.output_dir
+        
+    return args.mode, args.input_dir, odir, args.max_items    
+
+def annotate_item(atem, item, labels, is_training_sample=True, append_to_file=False):
+
+    atem["word_tokens"]["headers"].append("true-label")
+
+    char_i = atem["word_tokens"]["headers"].index("char_i")
+    char_j = atem["word_tokens"]["headers"].index("char_j")
+    
+    label="null"
+    for ri,row_i in enumerate(atem["word_tokens"]["data"]):
+        atem["word_tokens"]["data"][ri].append(label)
+
+    text = atem["text"]
+        
+    for key,vals in item.items():
+
+        if key in labels:
+
+            ranges=[]
+            
+            ind=0
+            for val in vals:
+                ind = text.find(val, ind)
+
+                if ind!=-1:
+                    ranges.append([ind, ind+len(val)])
+                else:
+                    break
+
+            for rng in ranges:
+                for ri,row_i in enumerate(atem["word_tokens"]["data"]):
+                    if rng[0]<=row_i[char_i] and row_i[char_j]<=rng[1]:
+                        atem["word_tokens"]["data"][ri][-1] = key
+
+    """
+    print(text)
+    print("\n\n", tabulate(atem["word_tokens"]["data"],
+                           headers=atem["word_tokens"]["headers"]))
+    """
+    
+    atem["annotated"]=True
+
+    #exit(-1)
+    
+    return atem
+    
+def prepare_crf(rfile, ofile, max_items, is_training_sample=True, append_to_file=False):
+
+    nlp_model = init_nlp_model("language", filters=["properties", "word_tokens"])
+
+    num_lines = sum(1 for _ in open(rfile))
+    if max_items!=-1:
+        max_items = min(max_items, num_lines)
+    else:
+        max_items = num_lines
+    
+    refs=[]
+
+    fr = open(rfile, "r")
+    if append_to_file:
+        fw = open(ofile, "a")
+    else:
+        fw = open(ofile, "w")
+
+    cnt = 0
+    
+    for i in tqdm.tqdm(range(0,max_items)):
+
+        line = fr.readline().strip()
+        if line==None or len(line)==0:
+            break
+
+        item = json.loads(line)
+        atem = nlp_model.apply_on_text(item["text"])
+        
+        atem = annotate_item(atem, item, labels=["chemicals"])
+        atem["training-sample"] = is_training_sample
+        
+        if "annotated" in atem and atem["annotated"]:
+            fw.write(json.dumps(atem)+"\n")
+            
+        cnt += 1
+
+def train_crf(train_file, model_file, metrics_file):
+
+    #model = andromeda_nlp.nlp_model()
+    model = nlp_model()
+                
+    configs = model.get_train_configs()
+
+    for config in configs:
+        if config["mode"]=="train" and \
+           config["model"]=="reference":
+
+            config["files"]["model-file"] = model_file
+            config["files"]["train-file"] = train_file
+            config["files"]["metrics-file"] = metrics_file 
+    
+            model.train(config)
+        
+def create_crf_model(mode, idir, odir, max_items):
+
+    train_file = os.path.join(idir, "train.jsonl")
+    test_file = os.path.join(idir, "test.jsonl")    
+    
+    #sfile = os.path.join(odir, "nlp-references.data.jsonl")    
+    afile = os.path.join(odir, "nlp-data.annot.jsonl")
+
+    crf_model_file = os.path.join(odir, "crf_model")
+    crf_metrics_file = crf_model_file+".metrics.txt"
+    
+    if mode=="prepare" or mode=="all":
+        prepare_crf(train_file, afile, max_items, is_training_sample=True, append_to_file=False)
+        prepare_crf(test_file, afile, max_items, is_training_sample=False, append_to_file=True)
+        
+    if mode=="train" or mode=="all":
+        train_crf(afile, crf_model_file, crf_metrics_file)
+        
+if __name__ == '__main__':
+
+    mode, idir, odir, max_items = parse_arguments()
+
+    create_crf_model(mode, idir, odir, max_items)
