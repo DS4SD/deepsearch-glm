64130733b6e23694efb0419650a142cd94b0f388
Peter Staar <taa@zurich.ibm.com>
2024-12-05 08:51:15 +0100

    reformatted the code
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_simple_interface.py
+++ tests/test_simple_interface.py
@@ -16,9 +16,9 @@ def test_simple_interface_v1():
     doc = json.load(open("tests/data/docs/1806.02284.json"))
     output = model.apply_on_doc(doc)
 
+
 def test_simple_interface_v2():
     model = nlp_model(loglevel="warning", text_ordering=True)
 
     doc = json.load(open("tests/data/docs/1806.02284.json"))
     output = model.apply_on_doc(doc)
-    
