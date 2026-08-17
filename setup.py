b30f0a637b6b87d0c67d4d4e3c644e72fa53d06f
Peter Staar <taa@zurich.ibm.com>
2026-08-17 18:43:06 +0200

    updated the ci
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+
+from setuptools import Distribution, find_packages, setup
+from setuptools.command.build_py import build_py as _build_py
+
+
+class CustomBuildPy(_build_py):
+    def run(self):
+        subprocess.check_call([sys.executable, "build.py"])
+        super().run()
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+setup(
+    packages=find_packages(include=["deepsearch_glm", "deepsearch_glm.*"]),
+    distclass=BinaryDistribution,
+    cmdclass={"build_py": CustomBuildPy},
+    zip_safe=False,
+    include_package_data=True,
+    package_data={
+        "deepsearch_glm": [
+            "*.so",
+            "*.pyd",
+            "*.dll",
+            "resources/**/*",
+        ],
+    },
+)
